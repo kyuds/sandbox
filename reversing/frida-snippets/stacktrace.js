ffe5059ee69ad5a4085fae516c186fcff8d8e3bf
Daniel Shin <88547237+kyuds@users.noreply.github.com>
2023-12-06 13:22:21 +0900

    .

--- reversing/frida-snippets/stacktrace.js
+++ reversing/frida-snippets/stacktrace.js
@@ -1,16 +1,37 @@
-// Native function hook
-Interceptor.attach(Module.findExportByName(null, "open"), {
-    onEnter: function(args) {
-        var backtrace = Thread.backtrace(this.context, Backtracer.ACCURATE)
-                              .map(DebugSymbol.fromAddress)
-                              .join("\n\t");
-        console.log(backtrace)
+// Android Specific. Get stacktrace like a Java exception.
+function get_android_stacktrace() {
+    return Java.use("android.util.Log")
+               .getStackTraceString(Java.use("java.lang.Exception")
+               .$new())
+}
+
+// To place into Frida's native function interceptor.
+// ctx (context) here is simply "this" that is provided
+// by the interceptor scope. 
+function get_native_stacktrace(ctx) {
+    return Thread.backtrace(ctx.context, Backtracer.ACCURATE)
+                 .map(DebugSymbol.fromAddress)
+                 .join("\n\t")
+}
+
+// Sample usecases:
+Java.perform(function() {
+    Java.use("java.lang.String").toString.implementation = function() {
+        console.warn("java function called")
+        console.log(get_android_stacktrace())
+        return this.toString()
     }
 })
 
-// Java function hook
-function get_stacktrace() {
-    console.log(Java.use("android.util.Log")
-                    .getStackTraceString(Java.use("java.lang.Exception")
-                    .$new()))
-}
+Interceptor.attach(Module.findExportByName(null, "open"), {
+    onEnter: function(args) {
+        console.warn("native function called")
+        // here "this" is provided by the Frida Interceptor.
+        // "this" can also be used to share state between
+        // onEnter and onLeave
+        console.log(get_native_stacktrace(this))
+    },
+    onLeave: function(retval) {
+        console.warn("leaving native function")
+    }
+})
