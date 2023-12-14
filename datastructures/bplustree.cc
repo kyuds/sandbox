facffe35834a1cc763b18e51319df890d6b4431d
Daniel Shin <kyuseung1016@gmail.com>
2023-12-14 11:33:04 +0900

    b+ tree: finish leafnode

--- datastructures/bplustree.cc
+++ datastructures/bplustree.cc
@@ -1,9 +1,67 @@
 #include "bplustree.hh"
 
-BPlusTree::BPlusTree() {
+// g++ -std=c++20 bplustree.cc -o bplustree
 
+Node::Node(bool _leaf, int _order) {
+    leaf = _leaf;
+    order = _order;
 }
 
-BPlusTree::~BPlusTree() {
-    
+LeafNode::LeafNode(int _order) : Node(true, _order) {
+    nextLeaf = NULL;
+}
+
+LeafNode::LeafNode(int _order, std::vector<pair_t> p) : Node(true, _order) {
+    kvStore = std::move(p);
+}
+
+Node * LeafNode::insert(long key, long val) {
+    for (auto p = kvStore.begin(); p != kvStore.end(); p++) {
+        if (p->key > key) {
+            kvStore.emplace(p, pair_t{ key, val });
+            break;
+        }
+    }
+    if (kvStore.size() <= getOrder() * 2) {
+        return NULL;
+    } else {
+        std::vector<pair_t> half;
+        half.insert(half.end(), std::make_move_iterator(kvStore.begin() + getOrder()),
+                                std::make_move_iterator(kvStore.end()));
+        kvStore.erase(kvStore.begin() + getOrder(), kvStore.end());
+        LeafNode * newLeaf = new LeafNode(getOrder(), std::move(half));
+        
+        newLeaf->setNextLeaf(nextLeaf);
+        nextLeaf = newLeaf;
+
+        return newLeaf;
+    }
+}
+
+void LeafNode::remove(long key) {
+    for (auto p = kvStore.begin(); p != kvStore.end(); p++) {
+        if (p->key == key) {
+            kvStore.erase(p);
+            return;
+        }
+    }
+}
+
+long LeafNode::getKey(long key) {
+    for (auto p = kvStore.begin(); p != kvStore.end(); p++) {
+        if (p->key == key) return p->val;
+    }
+    return NULL;
+}
+
+// testing code
+void testLeafNode();
+
+int main(void) {
+    testLeafNode();
+    return 0;
+}
+
+void testLeafNode() {
+
 }
--- datastructures/bplustree.hh
+++ datastructures/bplustree.hh
@@ -1,10 +1,94 @@
 #ifndef BPLUSTREE
 #define BPLUSTREE
 
-class BPlusTree {
+#include <vector>
+
+class Node;
+class LeafNode;
+class InnerNode;
+
+typedef struct {
+    long key;
+    long val;
+} pair_t;
+
+// class BPlusTree {
+//     public:
+//         BPlusTree(int _order);
+//         BPlusTree(int _order, std::vector<long>& keys, std::vector<long>& vals);
+//         ~BPlusTree();
+
+//         void insert(long key, long val);
+//         void remove(long key);
+
+//         long get(long key);
+//         long getOrDefault(long key, long val);
+
+//         int getOrder();
+//         int getHeight();
+
+//         // iterator stuff
+    
+//     private:
+//         int order;
+//         int height;
+//         Node * root;
+// };
+
+class Node {
     public:
-        BPlusTree();
-        ~BPlusTree();
+        Node(bool _leaf, int _order);
+        virtual ~Node() {}
+
+        bool isLeaf() { return leaf; }
+        int getOrder() { return order; }
+
+        virtual LeafNode * get(long key) = 0;
+        virtual LeafNode * getLeftMost() = 0;
+
+        virtual Node * insert(long key, long val) = 0;
+        virtual void remove(long key) = 0;
+        
+    private:
+        bool leaf;
+        int order;
+};
+
+// class InnerNode : public Node {
+//     public:
+//         InnerNode(int _order);
+//         ~InnerNode();
+
+//         LeafNode * get(long key) override;
+//         LeafNode * getLeftMost() override;
+
+//         void insert(long key, long val) override;
+//         void remove(long key) override;
+//     private:
+
+// };
+
+class LeafNode : public Node {
+    public:
+        LeafNode(int _order);
+        LeafNode(int _order, std::vector<pair_t> p);
+        ~LeafNode() {}
+
+        LeafNode * get(long key) override { return this; }
+        LeafNode * getLeftMost() override { return this; }
+
+        Node * insert(long key, long val) override;
+        void remove(long key) override;
+
+        long getKey(long key);
+        LeafNode * getNextLeaf() { return nextLeaf; }
+        void setNextLeaf(LeafNode * ln) { nextLeaf = ln; }
+
+        std::vector<pair_t>& getKeys() { return kvStore; }
+    
+    private:
+        LeafNode * nextLeaf;
+        std::vector<pair_t> kvStore;
 };
 
 #endif
