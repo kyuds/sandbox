798dec4887da69d1aae4a50f45c7e95a815c2d16
Daniel Shin <kyuseung1016@gmail.com>
2023-12-14 16:24:59 +0900

    todo: iterator + bulk

--- datastructures/bplustree.cc
+++ datastructures/bplustree.cc
@@ -1,6 +1,49 @@
 #include "bplustree.hh"
 
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+#include <utility>
+
 // g++ -std=c++20 bplustree.cc -o bplustree
+
+BPlusTree::BPlusTree(int _order) {
+    assert(_order > 0);
+    order = _order;
+    root = (Node *) new LeafNode(order);
+}
+
+void BPlusTree::put(long key, long val) {
+    auto r = root->put(key, val);
+    if (r.has_value()) {
+        Node * nd = r.value().first;
+        long sepKey = r.value().second;
+        std::vector<Node *> nodes;
+        nodes.push_back(root);
+        nodes.push_back(nd);
+        std::vector<long> keys;
+        keys.push_back(sepKey);
+        root = (Node *) new InnerNode(order, std::move(keys), std::move(nodes));
+    }
+}
+
+void BPlusTree::remove(long key) {
+    root->remove(key);
+}
+
+std::optional<long> BPlusTree::get(long key) {
+    LeafNode * ln = root->get(key);
+    return ln->getKey(key);
+}
+
+long BPlusTree::getOrDefault(long key, long def) {
+    auto ret = get(key);
+    if (ret.has_value()) {
+        return *ret;
+    }
+    return def;
+}
 
 Node::Node(bool _leaf, int _order) {
     leaf = _leaf;
@@ -13,55 +56,223 @@ LeafNode::LeafNode(int _order) : Node(true, _order) {
 
 LeafNode::LeafNode(int _order, std::vector<pair_t> p) : Node(true, _order) {
     kvStore = std::move(p);
+    nextLeaf = NULL;
 }
 
-Node * LeafNode::insert(long key, long val) {
+std::optional<std::pair<Node*, long>> LeafNode::put(long key, long val) {
+    bool success = false;
     for (auto p = kvStore.begin(); p != kvStore.end(); p++) {
+        if (p->key == key) {
+            success = true;
+            p->val = val;
+            break;
+        }
         if (p->key > key) {
+            success = true;
             kvStore.emplace(p, pair_t{ key, val });
             break;
         }
     }
+    if (!success) kvStore.push_back(pair_t{ key, val });
+
     if (kvStore.size() <= getOrder() * 2) {
-        return NULL;
+        return std::nullopt;
     } else {
         std::vector<pair_t> half;
         half.insert(half.end(), std::make_move_iterator(kvStore.begin() + getOrder()),
                                 std::make_move_iterator(kvStore.end()));
         kvStore.erase(kvStore.begin() + getOrder(), kvStore.end());
+        long sepKey = half.at(0).key;
         LeafNode * newLeaf = new LeafNode(getOrder(), std::move(half));
         
         newLeaf->setNextLeaf(nextLeaf);
         nextLeaf = newLeaf;
 
-        return newLeaf;
+        return std::make_pair(newLeaf, sepKey);
     }
 }
 
-void LeafNode::remove(long key) {
+bool LeafNode::remove(long key) {
     for (auto p = kvStore.begin(); p != kvStore.end(); p++) {
         if (p->key == key) {
             kvStore.erase(p);
-            return;
+            return true;
         }
     }
+    return false;
 }
 
-long LeafNode::getKey(long key) {
+std::optional<long> LeafNode::getKey(long key) {
     for (auto p = kvStore.begin(); p != kvStore.end(); p++) {
         if (p->key == key) return p->val;
     }
-    return NULL;
+    return std::nullopt;
+}
+
+void LeafNode::print(int offset) {
+    for (auto p = kvStore.begin(); p != kvStore.end(); p++) {
+        std::cout << std::string(offset, ' ');
+        std::cout << "(" << p->key << ", " << p->val << ")" << std::endl;
+    }
+}
+
+InnerNode::InnerNode(int _order,
+                     std::vector<long> _keys, 
+                     std::vector<Node *> _nodes) : Node(false, _order) {
+    assert(_nodes.size() - _keys.size() == 1 && _nodes.size() > 0);
+    keys = std::move(_keys);
+    nodes = std::move(_nodes);
+    height = 0;
+    for (auto n = nodes.begin(); n != nodes.end(); n++) {
+        height = std::max(height, (*n)->getHeight() + 1);
+    }
+}
+
+InnerNode::~InnerNode() {
+    for (int i = 0; i < nodes.size(); i++) {
+        delete nodes.at(i);
+    }
+}
+
+LeafNode * InnerNode::get(long key) {
+    Node * n = findRelevantNode(key);
+    return n->get(key);
+}
+
+LeafNode * InnerNode::getLeftMost() {
+    return nodes.at(0)->getLeftMost();
+}
+
+std::optional<std::pair<Node*, long>> InnerNode::put(long key, long val) {
+    Node * n = findRelevantNode(key);
+    auto ret = n->put(key, val);
+
+    if (ret.has_value()) {
+        Node * nd = ret.value().first;
+        long sepKey = ret.value().second;
+
+        auto n = nodes.begin() + 1;
+        bool success = false;
+        for (auto k = keys.begin(); k != keys.end(); k++) {
+            if (*k > sepKey) {
+                keys.emplace(k, sepKey);
+                nodes.emplace(n, nd);
+                success = true;
+                break;
+            }
+        }
+        if (!success) {
+            keys.emplace(keys.end(), sepKey);
+            nodes.emplace(nodes.end(), nd);
+        }
+        if (keys.size() <= getOrder() * 2) {
+            return std::nullopt;
+        }
+        std::vector<long> skeys;
+        long promote = *(keys.begin() + getOrder());
+        skeys.insert(skeys.end(), std::make_move_iterator(keys.begin() + getOrder() + 1),
+                                  std::make_move_iterator(keys.end()));
+        keys.erase(keys.begin() + getOrder(), keys.end());
+        std::vector<Node*> snodes;
+        snodes.insert(snodes.end(), std::make_move_iterator(nodes.begin() + getOrder() + 1),
+                                    std::make_move_iterator(nodes.end()));
+        nodes.erase(nodes.begin() + getOrder() + 1, nodes.end());
+        Node * newInner = new InnerNode(getOrder(), std::move(skeys), std::move(snodes));
+
+        return std::make_pair(newInner, promote);
+    }
+    return std::nullopt;
+}
+
+bool InnerNode::remove(long key) {
+    Node * n = findRelevantNode(key);
+    return n->remove(key);
+}
+
+Node * InnerNode::findRelevantNode(long key) {
+    assert(nodes.size() - keys.size() == 1 && nodes.size() > 0);
+
+    int idx = 0;
+    for (auto k = keys.begin(); k != keys.end(); k++) {
+        if (*k > key) break;
+        idx += 1;
+    }
+    return nodes.at(idx);
+}
+
+void InnerNode::print(int offset) {
+    auto n = nodes.begin();
+    (*n)->print(offset + 2);
+    for (auto k = keys.begin(); k != keys.end(); k++) {
+        std::cout << std::string(offset, ' ');
+        std::cout << *k << std::endl;
+        (*++n)->print(offset + 2);
+    }
 }
 
 // testing code
 void testLeafNode();
+void testInnerNode();
+void testBPlusTree();
 
 int main(void) {
-    testLeafNode();
+    testBPlusTree();
     return 0;
 }
 
-void testLeafNode() {
+void testBPlusTree() {
+    auto bt = new BPlusTree(2);
+    for (int i = 0; i < 30; i++) {
+        bt->put((long) i, (long) i);
+    }
+    bt->remove(14);
+    bt->remove(15);
+    bt->print();
+}
 
+void testLeafNode() {
+    // ord = 2; max keys = 4
+    LeafNode * nd = new LeafNode(2);
+    for (int i = 0; i < 16; i += 4) {
+        // adding four keys -- check they don't split
+        assert(!nd->put((long) i, (long) i).has_value());
+    }
+    // add one more key -- check they split
+    auto ret = nd->put((long) 2, (long) 2);
+    assert(ret.has_value());
+    // keys: 0, 2, 4, 8, 12 --> 0, 2 | 4, 8, 12
+    assert(ret->second == 4);
+    for (int i = 0; i <= 2; i += 2) {
+        assert(nd->getKey((long) i).has_value());
+    }
+    assert(!nd->getKey(4L).has_value());
+    LeafNode * split = (LeafNode *) ret.value().first;
+    for (int i = 4; i <= 12; i += 4) {
+        assert(split->getKey((long) i).has_value());
+    }
+    assert(!split->getKey(2L).has_value());
+
+    assert(nd->getNextLeaf() == split);
+}
+
+void testInnerNode() {
+    LeafNode * n1 = new LeafNode(2);
+    for (int i = 0; i < 16; i += 4) {
+        n1->put((long) i, (long) i);
+    }
+
+    std::vector<Node *> nodes;
+    nodes.push_back((Node *) n1);
+    InnerNode * i = new InnerNode(2, std::vector<long>(), std::move(nodes));
+
+    for (int x = 0; x < 20; x++) {
+        long t = (long) (x * 2) + 1;
+        auto r = i->put(t, t);
+        if (r.has_value()) {
+            r.value().first->print(0);
+            break;
+        }
+    }
+    i->remove(8L);
+    i->print(0);
 }
--- datastructures/bplustree.hh
+++ datastructures/bplustree.hh
@@ -1,6 +1,7 @@
 #ifndef BPLUSTREE
 #define BPLUSTREE
 
+#include <optional>
 #include <vector>
 
 class Node;
@@ -12,29 +13,6 @@ typedef struct {
     long val;
 } pair_t;
 
-// class BPlusTree {
-//     public:
-//         BPlusTree(int _order);
-//         BPlusTree(int _order, std::vector<long>& keys, std::vector<long>& vals);
-//         ~BPlusTree();
-
-//         void insert(long key, long val);
-//         void remove(long key);
-
-//         long get(long key);
-//         long getOrDefault(long key, long val);
-
-//         int getOrder();
-//         int getHeight();
-
-//         // iterator stuff
-    
-//     private:
-//         int order;
-//         int height;
-//         Node * root;
-// };
-
 class Node {
     public:
         Node(bool _leaf, int _order);
@@ -42,31 +20,44 @@ class Node {
 
         bool isLeaf() { return leaf; }
         int getOrder() { return order; }
+        virtual int getHeight() = 0;
 
         virtual LeafNode * get(long key) = 0;
         virtual LeafNode * getLeftMost() = 0;
 
-        virtual Node * insert(long key, long val) = 0;
-        virtual void remove(long key) = 0;
+        virtual std::optional<std::pair<Node*, long>> put(long key, long val) = 0;
+        virtual bool remove(long key) = 0;
+
+        virtual void print(int offset) = 0;
         
     private:
         bool leaf;
         int order;
 };
 
-// class InnerNode : public Node {
-//     public:
-//         InnerNode(int _order);
-//         ~InnerNode();
+class InnerNode : public Node {
+    public:
+        InnerNode(int _order,
+                  std::vector<long> _keys, 
+                  std::vector<Node *> _nodes);
+        ~InnerNode();
 
-//         LeafNode * get(long key) override;
-//         LeafNode * getLeftMost() override;
+        LeafNode * get(long key) override;
+        LeafNode * getLeftMost() override;
+        int getHeight() override { return height; }
 
-//         void insert(long key, long val) override;
-//         void remove(long key) override;
-//     private:
+        std::optional<std::pair<Node*, long>> put(long key, long val) override;
+        bool remove(long key) override;
 
-// };
+        void print(int offset) override;
+
+    private:
+        Node * findRelevantNode(long key);
+
+        int height;
+        std::vector<long> keys;
+        std::vector<Node *> nodes;
+};
 
 class LeafNode : public Node {
     public:
@@ -76,11 +67,14 @@ class LeafNode : public Node {
 
         LeafNode * get(long key) override { return this; }
         LeafNode * getLeftMost() override { return this; }
+        int getHeight() override { return 1; }
 
-        Node * insert(long key, long val) override;
-        void remove(long key) override;
+        std::optional<std::pair<Node*, long>> put(long key, long val) override;
+        bool remove(long key) override;
 
-        long getKey(long key);
+        void print(int offset) override;
+
+        std::optional<long> getKey(long key);
         LeafNode * getNextLeaf() { return nextLeaf; }
         void setNextLeaf(LeafNode * ln) { nextLeaf = ln; }
 
@@ -89,6 +83,30 @@ class LeafNode : public Node {
     private:
         LeafNode * nextLeaf;
         std::vector<pair_t> kvStore;
+};
+
+class BPlusTree {
+    public:
+        BPlusTree(int _order);
+        // BPlusTree(int _order, std::vector<long>& keys, std::vector<long>& vals);
+        ~BPlusTree() { delete root; }
+
+        void put(long key, long val);
+        void remove(long key);
+
+        std::optional<long> get(long key);
+        long getOrDefault(long key, long def);
+
+        int getOrder() { return order; }
+        int getHeight() { return root->getHeight(); }
+
+        void print() { root->print(0); }
+
+        // iterator stuff
+    
+    private:
+        int order;
+        Node * root;
 };
 
 #endif
