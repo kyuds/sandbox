74d6332426851f9c715d0155e6bdc632489ee61f
Daniel Shin <kyuseung1016@gmail.com>
2023-12-17 22:43:44 +0900

    sema-cycle: progress

--- concurrency/sema-cycle.cc
+++ concurrency/sema-cycle.cc
@@ -1,3 +1,21 @@
 #include "sema-cycle.hh"
 
-// g++ -std=c++20 sema-cycle.cc -o semacycle
+#include <iostream>
+
+// g++ -std=c++20 -D_LIBCPP_DISABLE_AVAILABILITY sema-cycle.cc -o semacycle
+
+Executor::Executor(int _numWorkers) {
+
+}
+
+Executor::~Executor() {
+
+}
+
+void Executor::cycle() {
+
+}
+
+void Executor::tf(bool& joinNow, Worker * worker) {
+
+}
--- concurrency/sema-cycle.hh
+++ concurrency/sema-cycle.hh
@@ -2,9 +2,45 @@
 
 #include <semaphore>
 #include <thread>
+#include <vector>
+#include <utility>
 
-// g++ -std=c++20 sema-cycle.cc -o semacycle
+// g++ -std=c++20 -D_LIBCPP_DISABLE_AVAILABILITY sema-cycle.cc -o semacycle
+
+// objectives:
+// - designing new multithread model for fixing 
+//   Praph and developing multithreaded ACO
+// - mitigating against MacOS restrictions on 
+//   semaphores
 
 class Executor {
+    public:
+        class Worker {
+            public:
+                Worker() { counter = 0; }
+                ~Worker() {}
 
+                void work() { counter++; }
+                int getData() { return counter; }
+                std::counting_semaphore<1>& waker() { return wakeWorker; }
+                std::counting_semaphore<1>& signal() { return signalDone; }
+            
+            private:
+                int counter;
+                std::counting_semaphore<1> wakeWorker {0};
+                std::counting_semaphore<1> signalDone {0};
+        };
+
+    public:
+        Executor(int _numWorkers);
+        ~Executor();
+
+        void cycle();
+        static void tf(bool& joinNow, Worker * worker);
+
+    private:
+        int numWorkers;
+        bool joinNow;
+        std::vector<std::thread> threads;
+        std::vector<Worker*> workers;
 };
